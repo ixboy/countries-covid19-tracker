383467a70fcf69598a1d7297368ec7ee0931bce8
ixboy <ismaelixboy@gmail.com>
2021-12-02 20:24:58 +0100

    create constructor, implement state and add card and chart components with dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React, { Component } from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import fetchData from './components/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+    this.setState({ data });
+  }
+
+  render() {
+    const { data } = this.state;
+    console.log(data);
+    return (
+      <div className={styles.container}>
+        <header className="App-header">
+          <h1>Hello World</h1>
+        </header>
+        <Cards data={data} />
+        <Chart data={data} />
+        <CountryPicker data={data} />
+      </div>
+    );
+  }
 }
-
 export default App;
