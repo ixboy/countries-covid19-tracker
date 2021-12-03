d5536bfef4c33661c45e3656765abb685fdd7b56
ixboy <ismaelixboy@gmail.com>
2021-12-03 06:45:20 +0100

    refactor api to comunicate with store

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
-
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async () => {
+export const GET_COUNTRY = 'country/getCountry';
+export const GET_ALLCOUNTRIES = 'world/getWorldCountries';
+const API = 'https://disease.sh/v3/covid-19/countries';
+
+const fetchAllCountriesData = async () => {
   try {
     const {
       data: {
@@ -15,4 +18,20 @@ const fetchData = async () => {
   } catch (error) { return error; }
 };
 
-export default fetchData;
+const getCountryData = async (action) => {
+  let country;
+  let countries;
+
+  switch (action.type) {
+    case GET_ALLCOUNTRIES:
+      countries = await fetchAllCountriesData();
+      return countries;
+    case GET_COUNTRY:
+      country = await fetchCountryData();
+      return country;
+    default:
+      return 'Unrecognized action';
+  }
+};
+
+export default getCountryData;
