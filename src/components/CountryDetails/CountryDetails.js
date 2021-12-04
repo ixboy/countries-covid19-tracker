0a16a0d409e4a9dc102691a5f778069a0043cabe
ixboy <ismaelixboy@gmail.com>
2021-12-04 19:53:22 +0100

    refactor country details to show flag

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Cards from './Cards';
+import styles from './CountryDetails.module.css';
 
 const CountryDetails = () => {
   const { name: country } = useParams();
@@ -10,9 +11,11 @@ const CountryDetails = () => {
   ));
   console.log(countryInfo);
   return (
-    <div>
-      <h1>Hello from details</h1>
+    <div className={styles.container}>
       <h1>{country}</h1>
+      <div>
+        <img className={styles.image} src={countryInfo.countryInfo.flag} alt="Flag" />
+      </div>
       <Cards info={countryInfo} />
     </div>
   );
