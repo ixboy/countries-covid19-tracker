82b1b20b58fa99c2cd8b4b8ee878d7a6edfd5de2
ixboy <ismaelixboy@gmail.com>
2021-12-04 01:27:28 +0100

    refactor store configurations

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
-import worldReducer from './world/world';
+import thunk from 'redux-thunk';
 import countryReducer from './country/country';
+import worldCountriesReducer from './worldCountries/worldCountries';
 
-const reducers = combineReducers({ worldReducer, countryReducer });
+const reducers = combineReducers({ worldCountriesReducer, countryReducer });
 
 const store = createStore(
   reducers,
