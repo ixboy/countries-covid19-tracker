b7fca63edc3bf0e843569c776cf255b4c9ac4112
ixboy <ismaelixboy@gmail.com>
2021-12-04 04:31:34 +0100

    refactor App and style it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
 import { CountryDetails, ListCountries } from './components';
 
 const App = () => (
-  <div className={styles.container}>
-    <h1>Hello World</h1>
-    <Switch>
-      <Route path="/" component={ListCountries} exact />
-      <Route path="/country" component={CountryDetails} exact />
-    </Switch>
-  </div>
+  <Router>
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+      <Routes>
+        <Route exact path="/" element={<ListCountries />} />
+        <Route path="/countries/:name" element={<CountryDetails />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
