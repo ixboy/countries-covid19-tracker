d20fba00e2ce472cc43315e97e2df63766e9e9ff
ixboy <ismaelixboy@gmail.com>
2021-12-04 19:51:49 +0100

    add details to recovered card

--- src/components/CountryDetails/Cards.js
+++ src/components/CountryDetails/Cards.js
@@ -1,23 +1,25 @@
-// import {
-//   Card, CardContent, Grid, Typography,
-// } from '@material-ui/core';
-// import cx from 'classnames';
+/* eslint-disable no-unused-vars */
+import {
+  Card, CardContent, Grid, Typography,
+} from '@material-ui/core';
+import cx from 'classnames';
 import React from 'react';
-// import CountUp from 'react-countup';
+import CountUp from 'react-countup';
 import styles from './Card.module.css';
 
 const Cards = ({
   info: {
-    country, cases, countryInfo, recovered,
+    country, cases, updated, deaths, todayCases, todayDeaths,
+    countryInfo: { flag }, recovered, active, continent, population,
   },
 }) => {
-  console.log(country);
-  console.log(countryInfo);
   console.log(cases);
+  console.log(country);
+  console.log(active);
   console.log(recovered);
   return (
     <div className={styles.container}>
-      {/* <Grid container spacing={3} justifyContent="center">
+      <Grid container spacing={3} justifyContent="center">
         <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
@@ -25,21 +27,23 @@ const Cards = ({
               {country}
             </Typography>
             <Typography variant="h5">
-              <CountUp start={0} end={confirmed.value} duration={2.0} separator="," />
+              active cases:
+              <CountUp start={0} end={active} duration={2.0} separator="," />
             </Typography>
             <Typography color="textSecondary" />
             <Typography variant="body2">
-              Number of infected people
+              Number of infected people:
+              <CountUp start={0} end={cases} duration={2.0} separator="," />
             </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card)}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
             <Typography variant="h5">
-              <CountUp start={0} end={recovered.value} duration={2.0} separator="," />
+              <CountUp start={0} end={recovered} duration={2.0} separator="," />
             </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography color="textSecondary">{new Date(updated).toDateString()}</Typography>
             <Typography variant="body2">
               Number of recovered people
             </Typography>
@@ -49,15 +53,15 @@ const Cards = ({
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
             <Typography variant="h5">
-              <CountUp start={0} end={deaths.value} duration={2.0} separator="," />
+              <CountUp start={0} end={deaths} duration={2.0} separator="," />
             </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography color="textSecondary">{new Date(updated).toDateString()}</Typography>
             <Typography variant="body2">
               Number of deaths by Covid-19
             </Typography>
           </CardContent>
         </Grid>
-      </Grid> */}
+      </Grid>
     </div>
   );
 };
