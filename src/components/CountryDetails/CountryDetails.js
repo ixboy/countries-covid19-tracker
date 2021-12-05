116c8580604d626f5c43621ba1fe5773b0c49017
ixboy <ismaelixboy@gmail.com>
2021-12-05 10:40:18 +0100

    create component to display the details of the country

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { fetchCountries } from '../../redux/worldCountries/worldCountries';
 import Cards from './Cards';
 import styles from './CountryDetails.module.css';
 
 const CountryDetails = () => {
   const { name: country } = useParams();
-  const countryInfo = useSelector((state) => state.worldCountriesReducer.allCountries.find(
-    (obj) => obj.country === country,
-  ));
-  console.log(countryInfo);
+  let countryInfo;
+  const dispatch = useDispatch();
+  countryInfo = useSelector((state) => state.worldCountriesReducer.allCountries);
+  console.log('Hello here');
+  useEffect(() => {
+    if (!countryInfo) {
+      dispatch(fetchCountries());
+    }
+  }, []);
+
+  if (!countryInfo) return 'Loading...';
+  if (countryInfo.constructor.name === 'Array') {
+    countryInfo = countryInfo.find((obj) => obj.country === country);
+  }
   return (
     <div className={styles.container}>
       <h1>{country}</h1>
