f2d1442571ee2fd683d151fe7c6c696f2bc8dd43
ixboy <ismaelixboy@gmail.com>
2021-12-05 07:54:00 +0100

    edit App js and navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
-import { CountryDetails, ListCountries } from './components';
+import { CountryDetails, ListCountries, Navbar } from './components';
 
 const App = () => (
   <Router>
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<ListCountries />} />
         <Route path="/countries/:name" element={<CountryDetails />} />
