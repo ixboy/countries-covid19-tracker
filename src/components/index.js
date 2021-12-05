eb73379f21bf6e109a7dc849d9d5ac1005df5de4
ixboy <ismaelixboy@gmail.com>
2021-12-05 10:49:05 +0100

    add exports for navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as CountryDetails } from './CountryDetails/CountryDetails';
 export { default as ListCountries } from './ListCountries/ListCountries';
-// export { default as CountryPicker } from './CountryPicker/CountryPicker';
+export { default as Navbar } from './Navbar/Navbar';
