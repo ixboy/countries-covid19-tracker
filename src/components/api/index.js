c3080a0495fe7d4d6e08c1af7036bbedbc1a3cff
ixboy <ismaelixboy@gmail.com>
2021-12-05 10:45:12 +0100

    refactor api

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
-// const url = 'https://covid19.mathdro.id/api';
 
-export const GET_COUNTRY = 'country';
 export const GET_ALLCOUNTRIES = 'worldCountries';
 const API = 'https://disease.sh/v3/covid-19/countries';
 
 const fetchWorldCountriesData = async () => {
   try {
     const { data } = await axios.get(API);
-    console.log(data[0].country);
     return data;
   } catch (error) { return error; }
 };
