a18de2aba985f944da50d65ab911ac590ae8c031
ixboy <ismaelixboy@gmail.com>
2021-12-11 03:38:35 +0100

    fix eslint errors

--- src/__tests__/ListCountries.test.js
+++ src/__tests__/ListCountries.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import ListCountries from '../components/__mocks__/ListCountries.js';
+import ListCountries from '../components/__mocks__/ListCountries';
 import store from '../redux/configureStore';
 
 describe('Group of tests for countries page', () => {
