002ef120397b3ed2fa89f455731dfb64cea2313d
ixboy <ismaelixboy@gmail.com>
2022-03-15 18:48:45 +0300

    Updates

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CountryDetails } from './CountryDetails/CountryDetails';
-export { default as ListCountries } from './ListCountries/ListCountries';
-export { default as Navbar } from './Navbar/Navbar';
